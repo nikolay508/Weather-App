9ba0773ef293b709d9019995b74b8a47ce84d4c4
Nikolay Todorov <157305036+nikolay508@users.noreply.github.com>
2024-06-14 00:46:36 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const apiKey = "990859935d868b088c1aeb62b313b126";
 const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
-
+ 
 const searchBox = document.querySelector('.search input');
 const searchBtn = document.querySelector('.search button');
 const weatherIcon = document.querySelector('.weather-icon');
